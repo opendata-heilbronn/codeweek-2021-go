eab2b97d3b961991a73169c96ed3e21d4e6307ce
Lars Hick <lars.hick@eljoth.de>
2021-10-10 11:21:12 +0200

    Rename directories for code examples

--- snippets/04-values_variables/floats.go
+++ snippets/04-values_variables/floats.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func floats(){
 	//START OMIT
-	var a float32
+	var a float32  // ..., -1.25, -1.0, ..., 1.25, ..., 1.27, ...
 	var b float64
 	//END OMIT
 	fmt.Println(a,b)
---
+++ snippets/04-values_variables/strings.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func strings(){
+	//START OMIT
+	var a string 				// Initialisierung
+	a = "Ich bin ein Text" 		// Zuweisung
+
+	//END OMIT
+	fmt.Println(a)
+}
